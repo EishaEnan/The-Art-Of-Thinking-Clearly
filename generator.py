ce16b720a0e3a96a4737be2798d76fcb2fa6a78d
EishaEnan <62539280+EishaEnan@users.noreply.github.com>
2023-02-27 15:21:07 +0000

    Generator function returns connection set

--- generator.py
+++ generator.py
@@ -1,11 +1,10 @@
 #Generates a connections Set using references for each chapter from the raw_data module
 
-from raw_data import chapters
+def generator(chapters):
+    connections = set()
 
-connections = set()
-
-
-for i, chapter in enumerate(chapters):
-    for ref in chapter[1]:
-        if (i, ref-1) not in connections or (ref-1, i) not in connections:
-            connections.add((i, ref-1))
+    for i, chapter in enumerate(chapters):
+        for ref in chapter[1]:
+            if (i, ref-1) not in connections or (ref-1, i) not in connections:
+                connections.add((i, ref-1))
+    return connections
