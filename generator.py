a777c23475cef1f10a8d5e28d1e3dc28d54f694a
EishaEnan <62539280+EishaEnan@users.noreply.github.com>
2023-02-27 15:09:21 +0000

    Generator creates connection set for all the unique connections

---
+++ generator.py
@@ -0,0 +1,11 @@
+#Generates a connections Set using references for each chapter from the raw_data module
+
+from raw_data import chapters
+
+connections = set()
+
+
+for i, chapter in enumerate(chapters):
+    for ref in chapter[1]:
+        if (i, ref-1) not in connections or (ref-1, i) not in connections:
+            connections.add((i, ref-1))
